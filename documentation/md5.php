8f885d05995715f3bc141a452e6173ebea5f2966
Rio Suga Catra Pratama <riosugacatrapratama@yahoo.co.id>
2017-01-17 18:21:22 +0700

    no message

--- documentation/md5.php
+++ documentation/md5.php
@@ -2,6 +2,16 @@
 	$a = "Bismillah";
 	echo openssl_digest($a, 'sha512');
 	
+	$array1 =  array(
+		'nama' =>'roi',
+		'nama_pelajaran' =>'bahasa arab',
+		'nilai' => '76');
+	$array2 = array(
+		'nama' =>'rio',
+		'nama_pelajaran' =>'bahasa inggris',
+		'nilai' => '97');
 
+	$arrFix = array_merge($array1, $array2);
+	var_dump($arrFix);
 	// public function set
  ?>
